e5d24dfae314d6cf56609b85076644655cd53197
Rosa <munguiarosa13@gmail.com>
2017-07-20 16:24:44 -0400

    added giantPlanet class

---
+++ AbstractPlanetAssignment/GiantPlanet.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Rosa Munguia
+ Date: July 20 2017
+ Description:  GiantPlanet class
+ Version: 0.1- added giantPlanet class
+ */
+namespace AbstractPlanetAssignment
+{
+    class GiantPlanet : Planet
+    {
+        //Private Instance Variables
+        private string _type;
+
+        //Constructor
+        /// <summary>
+        /// main constructor for giantplanet class
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="diameter"></param>
+        /// 
+        public GiantPlanet(string name, double diameter, double mass, string type)
+            : base(name, diameter, mass)
+        {
+            this._type = type;
+
+
+        }
+        //public Methods
+        public bool HasMoons()
+        {
+            return (MoonCount > 0);
+
+        }
+
+        public bool HasRings()
+        {
+            return (RingCount > 0);
+        }
+
+    }
+}
