e3379b92879545f294e7fc4d2f58e6df9ada42bc
Rosa <munguiarosa13@gmail.com>
2017-07-26 16:13:40 -0400

    Created IHabitable Interface

---
+++ AbstractPlanetAssignment/IHabitable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/* Rosa Munguia
+  Date: July 26 2017
+ Description: IHabitable Interface
+ Version: 0.1- Created IHabitable Interface
+ */
+namespace AbstractPlanetAssignment
+{
+    public interface IHabitable
+    {
+        bool Habitable();
+    }
+}
