4736df3ef84782b7b9cef7ad4becdcf5b0420b67
Rosa <munguiarosa13@gmail.com>
2017-07-28 21:31:56 -0400

    created one more object each of giantplanet and terrestrial planet

--- AbstractPlanetAssignment/Program.cs
+++ AbstractPlanetAssignment/Program.cs
@@ -5,8 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 /*Rosa Munguia
  Date: July 28 2017
- Description:  Driver
- Version: 0.4- properly implemented waitForAnyKey method in program class
+ Description:  Driver Class
+ Version: 0.5- created one more object each of giantplanet and terrestrial planet
  */
 namespace AbstractPlanetAssignment
 {
@@ -22,17 +22,23 @@ namespace AbstractPlanetAssignment
         {
             Console.WriteLine("List of Giant Planets: ");
             Console.WriteLine("-------------------------");
-            GiantPlanet bigPlanet = new GiantPlanet("Acolia", 50000, 100, "Ice");
+            GiantPlanet bigPlanet = new GiantPlanet("Acolia", 50000, Math.Pow(4,6), "Ice");
             Console.WriteLine(bigPlanet.ToString());
+            Console.WriteLine();
+            GiantPlanet anotherBigPlanet = new GiantPlanet("Frelia", 350000, Math.Pow(5, 7), "Gas");
+            Console.WriteLine(anotherBigPlanet.ToString());
 
             // bigPlanet.HasMoons();
             Console.WriteLine();
 
             Console.WriteLine("List of Terrestrial Planets:");
             Console.WriteLine("-----------------------------");
-            TerrestrialPlanet hugePlanet = new TerrestrialPlanet("Dolerth", 320000, 250, true);
+            TerrestrialPlanet hugePlanet = new TerrestrialPlanet("Dolerth", 320000, Math.Pow(9,5), true);
             Console.WriteLine(hugePlanet.ToString());
-            //  Console.WriteLine();
+            Console.WriteLine();
+            TerrestrialPlanet giganticPlanet = new TerrestrialPlanet("Zyneroid", 550000, Math.Pow(12, 7), false);
+            Console.WriteLine(giganticPlanet.ToString());
+           
             Program.WaitForAnyKey();  
         }
        
